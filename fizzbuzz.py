870ec3a8556d7035262219a51dacad6405f191ec
Maggie  Little <maggielittle95@gmail.com>
2023-08-01 13:42:04 -0400

    code edits needed for def FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 # add your code here
 
 # Define FizzBuzz
-def Fizz(num):
+def FizzBuzz(num):
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
     elif num % 3 == 0:
@@ -13,5 +13,5 @@ def Fizz(num):
 
 # Define the range of numbers to iterate through
 for num in range(1, 101):
-    # Call the Fizz function to print the appropriate word or number
+    # Call the FizzBuzz function to print the appropriate word or number
     FizzBuzz(num)
